3a5aea4ae95cb1964948d5a42d05d447673c09b1
kaslufl <leallucasfonseca@gmail.com>
2022-06-04 15:19:44 -0300

    feat: add cors

--- src/main.py
+++ src/main.py
@@ -1,7 +1,15 @@
 from fastapi import FastAPI
 from src.routes.user import user
 from src.routes.auth import auth
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(user)
 app.include_router(auth)
