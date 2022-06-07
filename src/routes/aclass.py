3384a7c7571dbe9942344ed675996f7a8dea886f
kaslufl <leallucasfonseca@gmail.com>
2022-06-07 14:03:19 -0300

    fix: change class lecture return

--- src/routes/aclass.py
+++ src/routes/aclass.py
@@ -26,16 +26,10 @@ def get_class_by_id(class_id: uuid.UUID):
 @aclass.get("/{class_id}/lectures")
 def get_class_lectures(class_id: uuid.UUID):
     route = class_composer()
-    a_class = route.get_class_by_id(class_id)
     lectures = route.get_class_lectures(class_id)
-    if not aclass:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail="Class not found",
-        )
     if not lectures:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
             detail="Lectures not found",
         )
-    return {"class": a_class, "lectures": lectures}
+    return lectures
