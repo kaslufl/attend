f5b00831d9e0321861ae1e3981b002f956b155e4
kaslufl <leallucasfonseca@gmail.com>
2022-06-09 03:52:21 -0300

    fix: change date format

--- src/infra/entities/lecture.py
+++ src/infra/entities/lecture.py
@@ -1,7 +1,7 @@
 import uuid
 
 from sqlalchemy import Column, String, ForeignKey
-from sqlalchemy.dialects.postgresql import UUID, TIMESTAMP
+from sqlalchemy.dialects.postgresql import UUID, DATE
 from sqlalchemy.orm import relationship
 
 from src.infra.config.db_base import Base
@@ -11,6 +11,6 @@ class Lecture(Base):
     __tablename__ = 'lectures'
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4())
     content = Column(String)
-    date = Column(TIMESTAMP)
+    date = Column(DATE)
     class_id = Column(UUID(as_uuid=True), ForeignKey("classes.id"))
     aclass = relationship("Class")
